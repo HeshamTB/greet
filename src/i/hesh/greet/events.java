3190e38373548577ed54358a1cbd175b2fe6eaa9
HeshamTB <38921037+HeshamTB@users.noreply.github.com>
2018-05-02 21:53:30 +0300

    build folder

--- src/i/hesh/greet/events.java
+++ src/i/hesh/greet/events.java
@@ -17,21 +17,20 @@ public class events implements Listener {
 	public void player(PlayerJoinEvent e) { 
 		
 		Player p = e.getPlayer();
-		e.setJoinMessage(prefix + ChatColor.GRAY + p.getName().toLowerCase() + ChatColor.AQUA + " joined 5radl Server!");
 		String pName = p.getDisplayName();
+		e.setJoinMessage(prefix + ChatColor.GRAY + p.getName().toLowerCase() + ChatColor.AQUA + " joined 5radl Server!");
 		p.sendMessage(ChatColor.AQUA +"Hello " + pName +  ", Welcome to 5radl Server");
-		//p.sendMessage(ChatColor.AQUA +"Online Players are "+ Bukkit.getOnlinePlayers());
-		
-		
 		p.sendMessage(ChatColor.GRAY + "Online Players :");
+		
 		int i = 1;
 		for(Player player : Bukkit.getOnlinePlayers()) {			
 			p.sendMessage(ChatColor.RED + "" + i + "- " + player.getName());
 			i++;
 		}
-		p.sendMessage(ChatColor.AQUA +"Type"+ ChatColor.GREEN +" (/help greet) "+ ChatColor.AQUA+ "for list of new commands");
 		
+		p.sendMessage(ChatColor.AQUA +"Type"+ ChatColor.GREEN +" (/help greet) "+ ChatColor.AQUA+ "for list of new commands");
 	}
+	
 	@EventHandler
 	public void Quit(PlayerQuitEvent e ) {
 		Player p = e.getPlayer();
@@ -44,8 +43,6 @@ public class events implements Listener {
 		Bukkit.getWorld("world").setFullTime(1);
 		Bukkit.getConsoleSender().sendMessage(prefix +"Good Morning!");
 	}
-	
-	
-	
+
 }
 	
